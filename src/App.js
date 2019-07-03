daf31b288491b52869e3b373542a6dc636e3b9db
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-07-03 23:35:06 +0200

    input field and search added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 
-class App extends Component {
+export default class App extends Component {
+  state = { artistQuery: "" };
+
+  updateArtistQuery = event => {
+    console.log("event", event);
+  };
   render() {
     return (
       <div>
         <h1>Musik Spot</h1>
+        <input
+          onChange={this.updateArtistQuery}
+          placeholder="search for an Artist"
+        />
+        <button>Search</button>
       </div>
     );
   }
 }
-
-export default App;
